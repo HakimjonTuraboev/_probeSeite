931b562ea62ea585df5014e2b4e5149ec19dfbda
hturab2s <46872070+HakimjonTuraboev@users.noreply.github.com>
2021-03-15 10:05:22 +0100

    uplod from pc 2

---
+++ build/src/main/java/im/hamroh/HamrohWebApplication.java
@@ -0,0 +1,13 @@
+package im.hamroh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamrohWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HamrohWebApplication.class, args);
+	}
+
+}
---
+++ build/src/test/java/im/hamroh/HamrohWebApplicationTests.java
@@ -0,0 +1,13 @@
+package im.hamroh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamrohWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
